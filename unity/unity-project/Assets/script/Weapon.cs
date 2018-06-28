0b4ce109063942dead8aa06015a0808b0910542e
Matthieu Giraud <matthieu.giraud99@gmail.com>
2018-06-28 21:39:14 +0200

    Add unity project and slideshows  (#1)
    * Add unity project and slideshows

---
+++ unity/unity-project/Assets/script/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    private GameObject parent;
+
+	void Start () {
+        parent = transform.parent.gameObject;	
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject != parent)
+        {
+            var player = collision.gameObject.GetComponent<PlayerMove>();
+            if(player)
+            {
+                player.hit();
+            }
+        }
+    }
+}
